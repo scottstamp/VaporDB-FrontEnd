1d59fc10e89c9bb5ed42ce63395f66c3623b0901
Scott Stamp <scott@hypermine.com>
2015-04-18 12:34:16 -0230

    Sinatra structure

---
+++ app/controller/application_controller.rb
@@ -0,0 +1,14 @@
+# application_controller.rb
+# VaporDB Frontend Mock-up
+# Author: Scott Stamp <scott@hypermine.com>
+# Date: 04/18/2015
+
+class ApplicationController < Sinatra::Base
+  helpers ApplicationHelper
+
+  set :views, File.expand_path('../../views', __FILE__)
+
+  configure :production, :development do
+    enable :logging
+  end
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+# application_helper.rb
+# VaporDB Frontend Mock-up
+# Author: Scott Stamp <scott@hypermine.com>
+# Date: 04/18/2015
+
+class ApplicationHelper
+
+end
